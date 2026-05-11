e03c35ebb0547a6b090f1289770178f28d92bc79
TEKT23 <iikbarrs@gmail.com>
2026-05-12 02:21:26 +0700

    fix: clean up seeder to remove dummy data logic

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,13 +22,5 @@ class DatabaseSeeder extends Seeder
             'email' => 'admin@pustaka.com',
             'password' => bcrypt('password'),
         ]);
-
-        \App\Models\Student::factory(50)->create()->each(function ($student) {
-            // Randomly create 1-10 attendance records for each student
-            \App\Models\Attendance::factory(rand(1, 10))->create([
-                'student_id' => $student->id,
-                'scanned_at' => now()->subDays(rand(0, 30))->subHours(rand(0, 23)),
-            ]);
-        });
     }
 }
